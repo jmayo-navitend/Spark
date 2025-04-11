8a2f3685751c77104d4245c87de734afc6d8498f
Johnny Mayo <jmayo@navitend.com>
2025-04-11 09:24:37 -0400

    Removed boilerplate code

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,1 @@
 console.log("✅ Preload script loaded");
-
-const { contextBridge } = require("electron");
-
-contextBridge.executeInMainWorld("electron", {
-  // Expose a function or variable to the main world
-});
