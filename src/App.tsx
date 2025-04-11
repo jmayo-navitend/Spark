52ea76f946ad530776157bcdace4c5b4f98d69fa
Johnny Mayo <jmayo@navitend.com>
2025-04-11 10:44:02 -0400

    Custom default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import reactLogo from "/react.svg";
+import electronLogo from "/electron.svg";
+import tailwindcssLogo from "/tailwindcss.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="text-stone-800 dark:text-stone-200 ">
+      <div className="flex justify-center gap-4">
+        <a href="https://vite.dev">
+          <img
+            src={viteLogo}
+            className="logo vite animate-pulse"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev">
+          <img
+            src={reactLogo}
+            className="logo react animate-[spin_20s_linear_infinite]"
+            alt="React logo"
+          />
+        </a>
+        <a href="https://www.electronjs.org/">
+          <img
+            src={electronLogo}
+            className="logo electron animate-bounce"
+            alt="Electron logo"
+          />
+        </a>
+        <a href="https://tailwindcss.com/">
+          <img
+            src={tailwindcssLogo}
+            className="logo tailwindcss animate-[ping_2s_ease-in-out_infinite] scale-80"
+            alt="Tailwind CSS logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="text-6xl font-medium">Vite + React + Electron </h1>
+      <h2 className="text-2xl mt-4">with Tailwind CSS</h2>
+      <div className="p-8 flex flex-col gap-4 place-items-center">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+
+        <p className="text-stone-500">
+          Click on the Vite, React, Electron, and Tailwind CSS logos to learn more
+        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
